8d9207ec357d95ffd5214424c906f7ec760a37ec
Dariusz Legizynski <DLegizynski@anexia-it.com>
2024-04-25 20:53:24 +0200

    fix mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import Link from "next/link"
 
 import Logo from "@/components/home/Logo"
-import IntroButton from "@/components/home/IntroButton"
 
 const Header = () => {
 	return (
-		<header className="grid grid-cols-[auto_auto] gap-x-8 w-full h-12 z-10 backdrop-blur-md">
-			<section className="max-w-[62rem] grid grid-cols-[1fr_auto_1fr] h-12 items-center justify-items-center px-4 mx-auto fadeInFromTop">
+		<header className="grid grid-cols-1 w-full h-12 z-10 backdrop-blur-md">
+			<section className="max-w-[62rem] grid grid-cols-[1fr_1fr] h-12 items-center justify-items-between w-full px-4 mx-auto fadeInFromTop">
 				<Logo />
 				<Link
 					className={`text-white justify-self-end`}
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export const Hero = () => {
 	return (
-		<article className="hero w-[60vw] h-[60vh] flex flex-col items-center p-3 text-white">
+		<motion.article className="flex w-[60vw] flex-col text-white relative">
 			{/* <section className="z-10">
 				<h1>
 					Drogi
@@ -14,23 +14,24 @@ export const Hero = () => {
 				<h2>Domowego Kościoła</h2>
 				<p>w Wiedniu</p>
 			</section> */}
-
-			<section className="flex flex-col items-center h-full justify-center relative">
+			<section>
 				<motion.div
-					className="absolute top-0 -left-6 w-[36vw] z-0 border-black border-[3rem]"
-					initial={{ height: 0, width: 0, opacity: 0.5 }}
-					animate={{ height: "30vw", width: "36vw", opacity: 1 }}
+					className="h-[60vh]"
+					initial={{ width: "0" }}
+					animate={{ width: "28vh" }}
 					transition={{ type: "spring", duration: 6 }}
-				></motion.div>
+				>
+					<div className="border-black border-[1.4rem] relative z-10 h-full w-full" />
+				</motion.div>
 				<Image
-					className="z-1 w-auto h-auto relative"
+					className="z-20 w-full h-auto flex flex-col absolute top-1/4 left-1"
 					src="/images/marriage.png"
 					alt="image of a couple in love"
 					width={640}
 					height={480}
 				/>
 			</section>
-		</article>
+		</motion.article>
 	)
 }
 
