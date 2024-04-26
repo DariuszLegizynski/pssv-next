71b2e0b3ab22873c8a81d5900dd6c5115a57b3ca
Dariusz Legizynski <DLegizynski@anexia-it.com>
2024-04-26 09:41:34 +0200

    change hero photo

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,25 +3,24 @@ import Image from "next/image"
 
 export const Hero = () => {
 	return (
-		<motion.article className="w-[60vw] h-full relative">
+		<article className="w-[60vw] h-[60vh] relative">
 			{/* <section className="z-10">
 				<h1>Hero</h1>
 			</section> */}
-			<section
-				className="flex flex-col items-center h-[60vh] max-w-[22rem] border-black border-[1.4rem] z-10 mx-auto"
+			<motion.div
+				className="flex flex-col items-center h-[60vh] max-w-[70%] border-black border-[1.8rem] z-10 absolute right-0"
 				initial={{ width: "0" }}
 				animate={{ width: "100%" }}
 				transition={{ type: "spring", duration: 6 }}
-			>
-				<Image
-					className="absolute z-20 w-96 h-auto top-1/3"
-					src="/images/marriage.png"
-					alt="image of a couple in love"
-					width={640}
-					height={480}
-				/>
-			</section>
-		</motion.article>
+			></motion.div>
+			<Image
+				className="absolute z-20 w-full h-auto -top-4 right-7"
+				src="/images/soldier/soldier-1.png"
+				alt="image of a couple in love"
+				width={640}
+				height={480}
+			/>
+		</article>
 	)
 }
 
