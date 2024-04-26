d70fdcc6f5ad4037223e9e327a30013937b30b8f
Dariusz Legizynski <DLegizynski@anexia-it.com>
2024-04-26 10:17:54 +0200

    adjust new hero picture

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,18 +3,18 @@ import Image from "next/image"
 
 export const Hero = () => {
 	return (
-		<article className="w-[60vw] h-[60vh] relative">
+		<article className="w-[60vw] h-[60vh] grid grid-cols-1 grid-rows-1 justify-items-end">
 			{/* <section className="z-10">
 				<h1>Hero</h1>
 			</section> */}
 			<motion.div
-				className="flex flex-col items-center h-[60vh] max-w-[70%] border-black border-[1.8rem] z-10 absolute right-0"
-				initial={{ width: "0" }}
+				className="grid-row-1 grid-col-1 h-[56vh] max-w-[60%] border-black border-[1.8rem] z-10"
+				initial={{ width: 0 }}
 				animate={{ width: "100%" }}
 				transition={{ type: "spring", duration: 6 }}
 			></motion.div>
 			<Image
-				className="absolute z-20 w-full h-auto -top-4 right-7"
+				className="grid-row-1 grid-col-1 z-20 w-full h-auto self-end pr-[1.8rem]"
 				src="/images/soldier/soldier-1.png"
 				alt="image of a couple in love"
 				width={640}
